af29f0421df06b4e81d7751a9b9ff791ded4e7eb
Amir Ernest <amir.m.ernest@gmail.com>
2022-07-01 21:37:20 +0200

    new post screen

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import * as React from 'react'
 import { SafeAreaView } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import GlobalStyles from './GlobalStyles';
+import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <HomeScreen/>
+      <NewPostScreen />
     </SafeAreaView>
   );
 }
---
+++ components/newPost/AddNewPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const AddNewPost = () => {
+  return (
+    <View>
+      <Text> AddNewPost </Text>
+    </View>
+  )
+}
+
+export default AddNewPost
