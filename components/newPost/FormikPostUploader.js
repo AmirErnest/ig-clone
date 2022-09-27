f8148419ab400eaa08f32047ff57b7322028883c
Amir Ernest <amir.m.ernest@gmail.com>
2022-09-27 23:03:16 +0200

    formik dynamic image url

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, Button } from 'react-native'
 import * as Yup from 'yup'
+import { Formik } from 'formik'
+import { Divider } from 'react-native-elements'
 
+const PLACEHOLDER_IMG = 'https://picsum.photos/id/870/200/300?grayscale&blur=2'
 
 const uploadPostSchema = Yup.object().shape({
   imageUrl: Yup.string().url().required('A URL is required'),
@@ -9,8 +12,71 @@ const uploadPostSchema = Yup.object().shape({
 })
 
 const FormikPostUploader = () => {
+   const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG)
+
   return (
-    <Text>FormikPostUploader</Text>
+    <Formik
+      initialValues={{caption: '', imageUrl: ''}}
+      onSubmit={(values) => console.log(values)}
+      validationSchema={uploadPostSchema}
+      validateOnMount={true}
+    >
+      {/* Formik form has a caption and url */}
+
+        {({ 
+          handleBlur,
+          handleChange, 
+          handleSubmit, 
+          values, 
+          errors, 
+          isValid 
+        }) => 
+            <>
+              <View style={{ margin: 20,
+                             justifyContent:'space-between', 
+                             flexDirection: 'row'
+              }}>
+
+                <Image 
+                  source={{uri: thumbnailUrl ? thumbnailUrl : PLACEHOLDER_IMG}} 
+                  style={{width: 100, height: 100}}
+                />
+
+                <View style={{ flex: 1, marginLeft: 12 }}>
+                  <TextInput
+                    style={{color: 'white', fontSize: 20}} 
+                    placeholder='Write a caption...' 
+                    placeholderTextColor='gray'
+                    multiline={true}
+                    onChangeText={handleChange('caption')}
+                    onBlur={handleBlur('caption')}
+                    value={values.caption}
+                  />
+                </View>
+              </View>
+
+              <Divider width={0.2} orientation='vertical'/>
+
+              <TextInput 
+                  onChange={(e) => setThumbnailUrl(e.nativeEvent.text)}
+                  style={{color: 'white', fontSize: 18}} 
+                  placeholder='Enter Image Url' 
+                  placeholderTextColor='gray'
+                  onChangeText={handleChange('imageUrl')}
+                  onBlur={handleBlur('imageUrl')}
+                  value={values.imageUrl}
+                />
+
+              {errors.imageUrl && (
+                <Text style={{ fontSize: 10, color: 'red' }}>
+                  {errors.imageUrl}
+                </Text>
+              )}
+
+              <Button onPress={handleSubmit} title='Share' disabled={!isValid} />
+            </>
+        }
+    </Formik>
   )
 }
 
